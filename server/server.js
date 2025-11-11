c81ed2eb5316c4695d7cd662d092ac0362d2ffe1
Aoey <109676807+Peteingthanin@users.noreply.github.com>
2025-11-11 15:48:16 +0700

    my ip

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = 3000;
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'Manow_wan@1234',
+    password: '1234',
     database: 'QMedicDB'
 });
 
