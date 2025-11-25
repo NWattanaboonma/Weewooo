5445f09e7caddbe179ee19932b7aefa4df335b5c
Nuttipong Wattanaboonma <nuttipong.wat@student.mahidol.ac.th>
2025-11-25 17:19:42 +0700

    update cypress

--- test/auto_test/cypress.config.ts
+++ test/auto_test/cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   },
   e2e: {
     // Point to the new spec file location
-    specPattern: "test/auto_test/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "test/auto_test/**/*.cy.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:8082",
     // Disable the support file since we are not using it yet
     supportFile: false,
