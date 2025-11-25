33bd1d8d850936cbc32eb0242b1ef11a18c8f9e6
Nuttipong Wattanaboonma <nuttipong.wat@student.mahidol.ac.th>
2025-11-25 17:45:40 +0700

    test for mook

--- test/auto_test/e2e/History.cy.ts
+++ test/auto_test/e2e/History.cy.ts
@@ -4,9 +4,12 @@ describe("App Navigation and Core Actions", () => {
     cy.visit("/");
   });
 
+  // Go to History Page test
   it("should navigate to the history page and display its content", () => {
     cy.contains("History").click();
     cy.url().should("include", "/history");
     cy.contains("History").should("be.visible");
+
   });
+  // 
 });
